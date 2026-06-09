8985c37cb78ac72d582c5b66908362bc14d40fee
Sidhish Kumar <sidhishkumar@gmail.com>
2026-06-09 09:13:51 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();  
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  return res.json({'message': 'Hello World!'} );
+});
+
+app.listen(8000, () => {
+  console.log('Example app listening on port 8000 ');
+}); 
