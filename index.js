8540f796b9297d41bc2063cdabfeb5cb293eab88
Sidhish Kumar <sidhishkumar@gmail.com>
2026-06-10 11:44:12 +0530

    github actions

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  return res.json({'message': 'Hello World!-- V2-- github actions--'} );
+  return res.json({'message': 'Hello World!-- V2-- github actions corrected--'} );
 });
 
 app.listen(8000, () => {
